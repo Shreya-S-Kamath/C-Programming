32c8334ddaae935f609e8a7f451473175d2fa0b5
Shreya-S-Kamath <66382057+Shreya-S-Kamath@users.noreply.github.com>
2020-06-29 21:23:31 +0530

    Create 2.c

---
+++ Swapping Numbers/2.c
@@ -0,0 +1,24 @@
+//Logic 2: Swapping two numbers using + and -
+
+#include<stdio.h> 
+void main() 
+{ 
+    int a,b; 
+    printf("Enter two numbers to be swapped:\n"); 
+    scanf("%d %d",&a,&b); 
+    a=a+b; 
+    b=a-b; 
+    a=a-b; 
+    printf("After swapping:\n%d\n%d",a,b); 
+    getch(); 
+} 
+ 
+/*
+Output:
+Enter two numbers to be swapped: 
+10 
+20 
+After swapping: 
+20 
+10 
+*/
