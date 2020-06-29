a6e1e102dfd52a7880df553556d4c462a349b424
Shreya-S-Kamath <66382057+Shreya-S-Kamath@users.noreply.github.com>
2020-06-29 22:25:52 +0530

    Update Hello World.c

--- Hello World.c
+++ Hello World.c
@@ -1,8 +1,10 @@
-#include <stdio.h>
-int main() 
+// Hello World program to show the basic syntax of C
+
+#include <stdio.h> // Standard input/output header file.
+int main() // Execution begins at main(). It is mandatory to have main().
 {
-   printf("Hello, World!");
-   return 0;
+   printf("Hello, World!"); // printf() sends formatted output to stdout.
+   return 0; // Returns 0 
 }
 
 /*
