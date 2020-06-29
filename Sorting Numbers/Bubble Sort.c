8b01dc76094e23e4b24b4ae734787af29038b193
Shreya-S-Kamath <66382057+Shreya-S-Kamath@users.noreply.github.com>
2020-06-29 21:57:38 +0530

    Create Bubble Sort.c

---
+++ Sorting Numbers/Bubble Sort.c
@@ -0,0 +1,41 @@
+// Bubble Sort
+
+#include<stdio.h> 
+int main() 
+{ 
+    int i,j,n,temp,a[100],low,high,key;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    printf("Enter the array elements:\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-1-i;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    printf("The array elements after sorting:\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",a[i]);
+    }
+    return 0;
+}
+
+/*
+Output:
+Enter the value of n:5
+Enter the array elements:
+12 678 456 34 99
+The array elements after sorting:
+12	34	99	456	 678	
+*/
